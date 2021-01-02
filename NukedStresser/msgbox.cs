c06b232e788d1a58621a8b30dd288ccf82fa7243
^^ Bianca ^^ <bianca_uwu@yahoo.com>
2021-01-02 02:34:04 +0100

    Create msgbox.cs

---
+++ NukedStresser/msgbox.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NukedStresser
+{
+    public partial class msgbox : Form
+    {
+        public msgbox()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
